8fe3c532e9d44cb77a2bb87bc05ba0aaece1fa16
Lucas Daves <lucas.goncalves@familiapires.com.br>
2025-05-02 17:51:03 -0300

    added swagger to project

--- src/models/user_model.ts
+++
@@ -1,9 +0,0 @@
-export class UserModel {
-  public id: string;
-  public name: string;
-
-  constructor(id: string, name: string) {
-    this.id = id;
-    this.name = name;
-  }
-}
--- src/routes/export_routes.ts
+++ src/routes/export_routes.ts
@@ -1,12 +1,13 @@
 import roleRoutes from "./role_routes";
-import profileRoutes from "./profile_routes";
-import userRoutes from "./user_routes";
 import { Router } from "express";
+
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "../../swagger.json";
 
 const router = Router();
 
+router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 router.use("/roles", roleRoutes);
-router.use("/profiles", profileRoutes);
-router.use("/users", userRoutes);
 
 export default router;
